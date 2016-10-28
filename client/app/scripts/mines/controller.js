4a2995ef82c2f30894e4c5bcbcc6ce4058837eb8
efrain17 <carlosefrain17@hotmail.com>
2016-10-28 17:53:41 -0500

    efrain

---
+++ client/app/scripts/mines/controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('Minesweeper')
+.controller('minesweeper', function ($scope) {
+  var matriz2 = [['*','',''],['','',''],['','*','']];
+  $scope.crearMatriz= matriz2;
+  // var mina2=0;
+   var filaMax=2;
+   var columMax=2;
+  $scope.controller_loaded = 'Minesweeper loaded!';
+  
+  $scope.seleccionarCelda = function(fila,columna){
+  	// return 1;
+  	 var acumuladorMina=0;
+  	// if((fila-1)>=0 && matriz2[fila-1][columna].toEqual('*')) mina2=mina2+1;
+  	// if((fila+1)<=filaMax && matriz2[fila+1][columna].toEqual('*')) mina2=mina2+1;
+  	
+  	if((columna-1)>=0 && matriz2[fila][columna-1]==='*'){
+  	  acumuladorMina=acumuladorMina+1;
+  	 }
+  	 console.log(acumuladorMina);
+
+  	if((fila+1)<=filaMax && matriz2[fila+1][columna]==='*')  acumuladorMina=acumuladorMina+1;
+  	console.log(acumuladorMina);
+  	if((fila-1)>=0 && (columna-1)>=0 && matriz2[fila-1][columna-1]==='*')  acumuladorMina=acumuladorMina+1;
+  	console.log(acumuladorMina);
+
+
+  	// if((columna+1)>=0 && matriz2[columna+1][fila].toEqual('*')) mina2=mina2+1; 
+ 
+  	 return acumuladorMina;
+  };
+
+})
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/minesweeper', {
+    templateUrl: 'scripts/mines/views/minesweeper.html',
+    controller: 'minesweeper'
+  });
+});
---
+++ client/app/scripts/mines/specs/controller_spec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+describe('Controller: minesweeper', function () {
+
+  beforeEach(module('Minesweeper'));
+  // beforeEach(module('underscore'));
+
+  var controller;
+  var scope;
+
+  beforeEach(inject(function ($rootScope, $controller) {
+    scope = $rootScope.$new();
+    controller = $controller('minesweeper', { $scope: scope });
+  }));
+
+  describe('On instance', function () {
+    it('should set "controller_loaded" variable in scope', function () {
+      expect(scope.controller_loaded).toContain('loaded');
+    });
+  });
+
+   describe('reglas del juego', function (){
+    it('crear matriz con dos minas', function(){
+
+      var matriz = [['*','',''],['','',''],['','*','']];
+      expect(scope.crearMatriz).toEqual(matriz);
+    });
+
+     it('seleccionar celda 2,2 y dar el valor de 1', function(){
+       
+        var mina=scope.seleccionarCelda(2,2);
+        expect(mina).toBe(1);
+    });
+
+     it('seleccionar celda 1,1 y dar el valor de 2', function(){
+       
+        var mina=scope.seleccionarCelda(1,1);
+        expect(mina).toBe(2);
+    });
+
+    it('seleccionar celda 0,2 y dar el valor de 0', function(){
+       
+        var mina=scope.seleccionarCelda(0,2);
+        expect(mina).toBe(0);
+    });
+
+
+   });
+
+  describe('when going to /minesweeper', function () {
+
+    var route, location, rootScope, httpBackend;
+
+    beforeEach(inject(function ($route, $location, $rootScope, $httpBackend) {
+      route = $route;
+      location = $location;
+      rootScope = $rootScope;
+      httpBackend = $httpBackend;
+
+      httpBackend.when('GET', 'scripts/mines/views/minesweeper.html').respond('<div></div>');
+    }));
+
+    afterEach(function () {
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should use minesweeper.html and controller', function () {
+      expect(route.current).toBeUndefined();
+
+      location.path('/minesweeper');
+
+      httpBackend.flush();
+
+      expect(route.current.templateUrl).toBe('scripts/mines/views/minesweeper.html');
+      expect(route.current.controller).toBe('minesweeper');
+    });
+  });
+
+});
